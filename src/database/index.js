d21f89df06400cf54a14e81bbaea10b680fd9012
abdurashidovtaha2 <abdurashidovtaha2@gmail.com>
2021-06-12 09:00:29 +0500

    heroku database settings

--- src/database/index.js
+++ src/database/index.js
@@ -1,19 +1,19 @@
 const MySql = require("mysql2");
-// const connection = MySQL.createPool({
-//   connectionLimit: 5,
-//   user: "bba5dc84ff37d5",
-//   password: "bbd46fcd",
-//   host: "us-cdbr-east-03.cleardb.com",
-//   database: "heroku_af4ae7528ff48ed",
-// });
-
-const connection = MySql.createPool({
-    connectionLimit: 10,
-    user: "root",
-    password: "root",
-    host: "127.0.0.1",
-    port: "3306",
-    database: "sml_db"
+const connection = MySQL.createPool({
+  connectionLimit: 5,
+  user: "b1e204a5b58247",
+  password: "ef0ab7f9",
+  host: "us-cdbr-east-04.cleardb.com",
+  database: "heroku_e6c4962d6553d26",
 });
+//mysql://b1e204a5b58247:ef0ab7f9@us-cdbr-east-04.cleardb.com/heroku_e6c4962d6553d26?reconnect=true
+// const connection = MySql.createPool({
+//     connectionLimit: 10,
+//     user: "root",
+//     password: "root",
+//     host: "127.0.0.1",
+//     port: "3306",
+//     database: "sml_db"
+// });
 
 module.exports = connection;
