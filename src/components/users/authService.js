b9252b65f46bf85e6827162ed1da1de7dc19cc33
abdurashidovtaha2 <abdurashidovtaha2@gmail.com>
2021-06-19 11:44:10 +0500

    usertype in login request

--- src/components/users/authService.js
+++ src/components/users/authService.js
@@ -12,14 +12,16 @@ class AuthService extends Service {
     async login(email, password) {
         try {
             const user = await DBQuery(`SELECT * FROM users WHERE email=${connection.escape(email)} AND verified=1`, true, "USER", statusCodes.NOT_FOUND);
-            const { id: userID, password: userPassword } = user[0];
+            const { id: userID, password: userPassword, admin } = user[0];
             const token = uuid();
 
             if (password !== userPassword) throw({ statusCode: statusCodes.FORBIDDEN, err: errorCodes.WRONG_PASSWORD });
 
             await DBQuery(`INSERT INTO tokens (id, user_id) VALUES (${connection.escape(token)}, ${connection.escape(userID)})`);
 
-            return { statusCode: statusCodes.OK, id: userID, token };
+            const userType = admin === 1 ? "admin" : "user";
+
+            return { statusCode: statusCodes.OK, id: userID, token, userType };
         } catch (err) {
             throw(err);
         }
