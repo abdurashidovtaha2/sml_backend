f0c4996fb382c5892c0b9d4c031d0e707a61c0af
abdurashidovtaha2 <abdurashidovtaha2@gmail.com>
2021-06-21 12:39:00 +0500

    frontend site url fixed

--- src/environment.js
+++ src/environment.js
@@ -1,7 +1,6 @@
 const environment = {
-    // port: "localhost:8000"
     port: "https://sml-backend.herokuapp.com",
-    url: "https://elegant-nightingale-be6767.netlify.app/"
+    url: "https://elegant-nightingale-be6767.netlify.app"
 };
 
 module.exports = environment;
